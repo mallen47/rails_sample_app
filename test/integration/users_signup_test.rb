6f08afecb5c504b675fa83d58c5637a9219fe948
Matt Allen <allen.matt@gmail.com>
2019-10-26 21:05:10 +0000

    Complete section 7.4.4 A Test for Valid Submission
    Created flash messaging to display upon user signup.
    Added integration test to check valid signup.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,4 +18,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: {name: "Test User",
+                                       email: "test@example.com",
+                                       password: "abc123",
+                                       password_confirmation: "abc123" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+  
 end
