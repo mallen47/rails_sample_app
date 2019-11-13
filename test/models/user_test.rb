725670460115f35884d2159e5173be82b6c09930
Matt Allen <allen.matt@gmail.com>
2019-11-13 16:01:02 +0000

    Complete section 9.1.4 Two subtle bugs
    Fix bugs dealing with 1.) attempting to logout against a nil user
    and 2.) attempting to logout user when remember_digest is nil
    
    The first happens when logged into two windows of one browser. Then
    logging out of the first window, then the second.
    
    The second issue happens when logged into two different browsers then
    logging out of browser 1, then logging out of browser 2, then reopen
    browser two and attempt to login.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,6 +6,11 @@ class UserTest < ActiveSupport::TestCase
                      password: "foobar", password_confirmation: "foobar")
   end
   
+  
+  test "authenticated? should return false for user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+  
   test "should be valid" do
     assert @user.valid?
   end
