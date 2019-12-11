f47b4521ba045926aadfb9a5ee96bebf8955eba9
Matt Allen <allen.matt@gmail.com>
2019-12-11 11:03:53 +0000

    12.1.3 Password reset create action
    Updates to lay the ground work for enabling password reset feature.
    Add create action to password_reset_controller and update the User
    model accordingly.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   
   before_save :downcase_email
   before_create :create_activation_digest
@@ -31,15 +31,29 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
   
+  # Send activatino email
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
   
+  # Activates an account
   def activate
     # update_attribute(:activated, true)
     # update_attribute(:activated_at, Time.zone.now)
     # the two lines above can be written as,
     update_columns(activated: true, activated_at: Time.zone.now)
+  end
+  
+  # Set the password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+  
+  # Send the password reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
   
   # class methods
