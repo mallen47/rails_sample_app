b2bb71cc0b901ee7833738d910228bf51f982e1d
Matt Allen <allen.matt@gmail.com>
2019-12-31 14:07:33 +0000

    13.1.3 User/Micropost associations
    Create relationship between user and micropost objects

--- db/migrate/20191230150354_create_microposts.rb
+++ db/migrate/20191230150354_create_microposts.rb
@@ -3,7 +3,6 @@ class CreateMicroposts < ActiveRecord::Migration[6.0]
     create_table :microposts do |t|
       t.text :content
       t.references :user, null: false, foreign_key: true
-      #t.references :user, foreign_key: true
 
       t.timestamps
     end
