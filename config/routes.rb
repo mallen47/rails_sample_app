e2ec1f729af5a6c1d810b32a007fc1a9e26c9e20
Matt Allen <allen.matt@gmail.com>
2020-01-04 16:24:21 +0000

    13.3.2 Creating Microposts
    Create form, partials and update controller to enable micropost
    creation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'password_resets/new'
   get 'password_resets/edit'
+  # get 'microposts/new', to: 'microposts#new'
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
