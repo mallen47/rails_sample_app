2f755188a38cbf76cc12db39d9d1542ec18876f2
Matt Allen <allen.matt@gmail.com>
2020-01-08 11:24:05 +0000

    13.3.4 Destroying microposts
    A user can now delete their own posts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,9 +1,8 @@
 class MicropostsController < ApplicationController
   
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
   
-  # def new
-  # end
   
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -17,11 +16,22 @@ class MicropostsController < ApplicationController
   end
   
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    # The 2 lines below accomplish the same thing
+    # redirect_to request.referrer || root_url
+    # this one was added in Rails 5
+    redirect_back(fallback_location: root_url)
   end
   
   private
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+    
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
   
 end
