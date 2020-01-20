0fec823ecd4b097c395ed98e317958cd51ac3508
Matt Allen <allen.matt@gmail.com>
2020-01-20 13:29:56 +0000

    14.1.3 Relationship validations
    Add relationship tests and fix broken ones by removing default fixture
    data. Also include optional relationship validations for completeness.

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,9 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+  # as of Rails 5 the following are not required - the tests would still pass
+  # without them - but are included for completeness. See tutorial ch 14.1.3
+  # Exercise 1.
+  validates  :follower_id, presence: true
+  validates  :followed_id, presence: true
 end
